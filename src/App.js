2a1160addb3382474b515c3329e7c5a7882584d3
cheb4 <a.splitas@gmail.com>
2021-01-15 19:05:11 +0200

    mockup before controlers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 // import logo from './logo.svg';
 // import './App.css';
-
+import Input from "./input";
+import Button from "./Button";
 function App() {
-  return <div>hello</div>;
+  const Welcome = () => {
+    return (
+      <div>
+        <h1>Welcome</h1>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Welcome />
+      <Input />
+      <Input />
+      <Button />
+      <Button />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/input.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Input() {
+  return (
+    <div>
+      <input type="text" name="inputField" id="input" />
+    </div>
+  );
+}
+
+export default Input;
