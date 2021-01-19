36aa358ef5399dc7b8432393d120207ed9a68c20
cheb4 <a.splitas@gmail.com>
2021-01-19 19:57:41 +0200

    moving components

--- src/App.js
+++ src/App.js
@@ -1,77 +1,17 @@
-// import logo from './logo.svg';
-// import './App.css';
-import Input from "./components/input";
-import Button from "./components/Button";
 import { useState } from "react";
-import logToConsole from "./helper/helpers";
-
+import Welcome from "./welcome";
 function App() {
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLoginInput = (event) => {
-    event.preventDefault();
-    logToConsole(event.target.value, "login");
-    setLogin(event.target.value);
-  };
-  const handlePasswordInput = (event) => {
-    event.preventDefault();
-    logToConsole(event.target.value, "password");
-    setPassword(event.target.value);
-  };
-
-  const loginHandler = () => {
-    console.log("somthing will happen here login");
-  };
-  const registerHandler = () => {
-    console.log("somthing will happen here register");
-  };
-
-  const Welcome = () => {
-    return (
-      <div>
-        <h1>Welcome</h1>
-      </div>
-    );
-  };
-
   return (
     <div>
-      <Welcome />
-      <Input
-        handler={handleLoginInput}
-        id={"loginField"}
-        placeholder="mock@mail.dev"
-        type="text"
-        name="loginField"
-        label="loginField"
+      <Welcome
+        login={login}
+        setLogin={setLogin}
+        password={password}
+        setPassword={setPassword}
       />
-      <Input
-        handler={handlePasswordInput}
-        id={"passwordField"}
-        type="password"
-        name="passwordField"
-        label="passwordField"
-      />
-      <Button
-        handler={loginHandler}
-        value="login"
-        id="loginButton"
-        type="button"
-        name="loginButton"
-        label="loginButton"
-      />
-      <Button
-        handler={registerHandler}
-        value="register"
-        id="loginButton"
-        type="button"
-        name="loginButton"
-        label="loginButton"
-      />
-      <br />
-      <div>login: {login}</div>
-      <div>password: {password}</div>
     </div>
   );
 }
---
+++ src/welcome.js
@@ -0,0 +1,76 @@
+import Input from "./components/input";
+import Button from "./components/Button";
+import logToConsole from "./helper/helpers";
+
+function Welcome(props) {
+  logToConsole(props, "props");
+  const { login, setLogin, password, setPassword } = props;
+
+  const handleLoginInput = (event) => {
+    event.preventDefault();
+    logToConsole(event.target.value, "login");
+    setLogin(event.target.value);
+  };
+  const handlePasswordInput = (event) => {
+    event.preventDefault();
+    logToConsole(event.target.value, "password");
+    setPassword(event.target.value);
+  };
+
+  const loginHandler = () => {
+    console.log("somthing will happen here login");
+  };
+  const registerHandler = () => {
+    console.log("somthing will happen here register");
+  };
+
+  const Welcome = () => {
+    return (
+      <div>
+        <h1>Welcome</h1>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Welcome />
+      <Input
+        handler={handleLoginInput}
+        id={"loginField"}
+        placeholder="mock@mail.dev"
+        type="text"
+        name="loginField"
+        label="loginField"
+      />
+      <Input
+        handler={handlePasswordInput}
+        id={"passwordField"}
+        type="password"
+        name="passwordField"
+        label="passwordField"
+      />
+      <Button
+        handler={loginHandler}
+        value="login"
+        id="loginButton"
+        type="button"
+        name="loginButton"
+        label="loginButton"
+      />
+      <Button
+        handler={registerHandler}
+        value="register"
+        id="loginButton"
+        type="button"
+        name="loginButton"
+        label="loginButton"
+      />
+      <br />
+      <div>login: {login}</div>
+      <div>password: {password}</div>
+    </div>
+  );
+}
+
+export default Welcome;
